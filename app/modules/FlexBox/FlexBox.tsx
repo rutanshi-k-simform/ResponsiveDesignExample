1c4b8f9c1af3d6927159633f516e14d744e2b776
Rutanshi Khambhatiya <rutanshi.k@simformsolutions.com>
2023-09-06 13:22:58 +0530

    feat: UNT-T14138: Change design of Flexbox Example

--- app/modules/FlexBox/FlexBox.tsx
+++ app/modules/FlexBox/FlexBox.tsx
@@ -1,17 +1,73 @@
-import {View} from 'react-native';
-import React from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import styles from './styles';
+import {Header} from '../../components';
+import {Metrics} from '../../theme';
+
+const {width, height} = Metrics;
+
+const card = ({item}: any) => {
+  return (
+    <View style={styles.cardContainer}>
+      <Image
+        source={{uri: item?.thumbnail ?? item?.images?.[0]}}
+        style={styles.image}
+        resizeMode="contain"
+      />
+      <View style={styles.textContainer}>
+        <Text style={styles.title} numberOfLines={1}>
+          {item?.title ?? ''}
+        </Text>
+        <Text style={styles.brand}>{item?.brand ?? ''}</Text>
+        <Text style={styles.description} numberOfLines={2}>
+          {item?.description ?? ''}
+        </Text>
+        <Text style={styles.price}>$ {item?.price ?? 0}.00</Text>
+      </View>
+    </View>
+  );
+};
+
+const emptyComponent = () => <Text>Oops! Something went wrong</Text>;
 
 const FlexBox = () => {
+  const [productList, setProductList] = useState({isLoading: true, data: []});
+
+  const fetchProducts = () => {
+    fetch('https://dummyjson.com/products')
+      .then(res => res.json())
+      .then(res =>
+        setProductList({data: res?.products ?? [], isLoading: false}),
+      )
+      .catch(() => setProductList({data: [], isLoading: false}));
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <View style={[styles.box, {flex: 1, backgroundColor: 'lightblue'}]} />
-      <View style={[styles.box, {flex: 2, backgroundColor: 'pink'}]} />
-      <View style={[styles.box, {flex: 3, backgroundColor: 'orange'}]} />
-      {/* <View style={[styles.box, {backgroundColor: 'black'}]} />
-      <View style={[styles.box, {backgroundColor: 'brown'}]} />
-      <View style={[styles.box, {backgroundColor: 'green'}]} /> */}
-    </View>
+    <SafeAreaView style={styles.mainContainer}>
+      <Header title="FlexBox" />
+      {productList.isLoading ? (
+        <ActivityIndicator style={{height}} size="large" />
+      ) : (
+        <FlatList
+          data={productList.data}
+          renderItem={card}
+          numColumns={Math.floor(width / 300)}
+          contentContainerStyle={styles.contentContainerStyle}
+          style={styles.flatList}
+          ListEmptyComponent={emptyComponent}
+        />
+      )}
+    </SafeAreaView>
   );
 };
 
--- app/modules/FlexBox/styles.ts
+++ app/modules/FlexBox/styles.ts
@@ -1,17 +1,68 @@
 import {StyleSheet} from 'react-native';
 import {Metrics} from '../../theme';
 
-const {height} = Metrics;
+const {
+  globalMetrics: {isWeb},
+} = Metrics;
 
 const styles = StyleSheet.create({
-  container: {
-    height: height,
-    // flexDirection: 'row',
-    // flexWrap: 'wrap',
+  mainContainer: {
+    flex: isWeb ? undefined : 1,
+    backgroundColor: '#fff',
   },
-  box: {
-    // height: 200,
-    width: 200,
+  flatList: {
+    flex: 1,
+    marginHorizontal: 25,
+  },
+  contentContainerStyle: {
+    gap: 10,
+    padding: 10,
+  },
+  cardContainer: {
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    elevation: 2,
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    borderRadius: 5,
+    flex: 1 / 4, // In tablet 1/2 flex and in web 1/4 flex
+    backgroundColor: 'white',
+  },
+  image: {
+    aspectRatio: 1,
+    flex: 0.75,
+  },
+  textContainer: {
+    padding: 10,
+    flex: 0.25,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '700',
+    color: 'black',
+    flex: 1,
+  },
+  brand: {
+    fontSize: 14,
+    fontWeight: '500',
+    color: 'purple',
+    flex: 1,
+  },
+  description: {
+    fontSize: 14,
+    fontWeight: '500',
+    color: 'black',
+    flex: 1.68,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'black',
+    alignSelf: 'center',
+    flex: 1.32,
   },
 });
 
