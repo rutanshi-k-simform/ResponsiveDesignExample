817a9992c3db1ec4dd5d891476e4d616f70b51ee
Rutanshi Khambhatiya <rutanshi.k@simformsolutions.com>
2023-09-06 13:22:33 +0530

    feat: UNT-T14138: Change design of Dimensions Example

--- app/modules/DimensionsExample/DimensionsExample.tsx
+++ app/modules/DimensionsExample/DimensionsExample.tsx
@@ -1,22 +1,78 @@
-import {View, Text, useWindowDimensions} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  useWindowDimensions,
+  SafeAreaView,
+  FlatList,
+  ActivityIndicator,
+  Image,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import styles from './styles';
 import {Header} from '../../components';
+import {Metrics} from '../../theme';
+
+const {
+  globalMetrics: {isWeb, isTablet},
+} = Metrics;
 
 const DimensionsExample = () => {
   const {width, height} = useWindowDimensions();
   const dimensionStyles = styles(width, height);
+  const [productList, setProductList] = useState({isLoading: true, data: []});
+  const fetchProducts = () => {
+    fetch('https://dummyjson.com/products')
+      .then(res => res?.json())
+      .then(res =>
+        setProductList({data: res?.products ?? [], isLoading: false}),
+      )
+      .catch(() => setProductList({data: [], isLoading: false}));
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const card = ({item}: any) => {
+    return (
+      <View style={dimensionStyles.cardContainer}>
+        <Image
+          source={{uri: item?.thumbnail ?? item?.images?.[0]}}
+          style={dimensionStyles.image}
+          resizeMode="contain"
+        />
+        <View style={dimensionStyles.textContainer}>
+          <Text style={dimensionStyles.title} numberOfLines={1}>
+            {item?.title ?? ''}
+          </Text>
+          <Text style={dimensionStyles.brand}>{item?.brand ?? ''}</Text>
+          <Text style={dimensionStyles.description} numberOfLines={2}>
+            {item?.description ?? ''}
+          </Text>
+          <Text style={dimensionStyles.price}>$ {item?.price ?? 0}.00</Text>
+        </View>
+      </View>
+    );
+  };
+
+  const emptyComponent = () => <Text>Oops! Something went wrong</Text>;
 
   return (
-    <View style={dimensionStyles.container}>
-      <View style={dimensionStyles.header}>
-        <Header title="Dimensions" />
-      </View>
-      <View style={dimensionStyles.body}>
-        <Text style={dimensionStyles.text}>This is Body</Text>
-      </View>
-      <View style={dimensionStyles.footer} />
-    </View>
+    <SafeAreaView style={dimensionStyles.mainContainer}>
+      <Header title="Dimensions" />
+      {productList.isLoading ? (
+        <ActivityIndicator style={dimensionStyles.loader} size="large" />
+      ) : (
+        <FlatList
+          data={productList.data}
+          renderItem={card}
+          numColumns={isWeb ? 4 : isTablet ? 2 : 1}
+          contentContainerStyle={dimensionStyles.contentContainerStyle}
+          ListEmptyComponent={emptyComponent}
+          style={dimensionStyles.flatListStyle}
+        />
+      )}
+    </SafeAreaView>
   );
 };
 
--- app/modules/DimensionsExample/styles.ts
+++ app/modules/DimensionsExample/styles.ts
@@ -1,30 +1,78 @@
 import {StyleSheet} from 'react-native';
+import {Metrics} from '../../theme';
+
+const {
+  globalMetrics: {isWeb, isTablet},
+} = Metrics;
 
 const styles = (width: number, height: number) =>
   StyleSheet.create({
-    container: {
-      width,
+    loader: {
       height,
     },
-    header: {
+    mainContainer: {
       width: width,
-      height: height * 0.1,
-      justifyContent: 'flex-end',
-      backgroundColor: '#B2D8EC',
+      backgroundColor: 'white',
     },
-    body: {
-      width: width,
-      height: height * 0.8,
-      justifyContent: 'center',
-      alignItems: 'center',
+    flatListStyle: {
+      width: width - 150,
+      alignSelf: 'center',
     },
-    footer: {
-      width: width,
-      height: height * 0.1,
-      backgroundColor: '#B2D8EC',
+    contentContainerStyle: {
+      gap: 10,
+      padding: width / 100,
     },
-    text: {
-      fontSize: 20,
+    cardContainer: {
+      shadowColor: 'black',
+      shadowOffset: {
+        width: 0,
+        height: 0,
+      },
+      elevation: 2,
+      shadowOpacity: 0.5,
+      shadowRadius: 3,
+      borderRadius: 5,
+      width: isTablet
+        ? (0.98 * width - 150) / 2
+        : isWeb
+        ? (0.98 * width - 150) / 4
+        : 0.98 * width - 150,
+      backgroundColor: 'white',
+    },
+    image: {
+      aspectRatio: 1,
+      width: isTablet
+        ? (0.98 * width - 150) / 2
+        : isWeb
+        ? (0.98 * width - 150) / 4
+        : 0.98 * width - 150,
+    },
+    textContainer: {
+      padding: 10,
+    },
+    title: {
+      fontSize: 18,
+      fontWeight: '700',
+      color: 'black',
+      marginBottom: 5,
+    },
+    brand: {
+      fontSize: 14,
+      fontWeight: '500',
+      color: 'purple',
+      marginBottom: 5,
+    },
+    description: {
+      fontSize: 14,
+      fontWeight: '500',
+      color: 'black',
+      marginBottom: 5,
+    },
+    price: {
+      fontSize: 19,
+      fontWeight: 'bold',
+      color: 'black',
+      alignSelf: 'center',
     },
   });
 
